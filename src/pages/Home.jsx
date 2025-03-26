74897195eb5399faa903e2a24d08819832b51325
MuskanDhaka <mdhaka@innow8apps.com>
2025-03-26 12:17:23 +0530

    basic ui component

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+    return(
+        <>
+        <h1>Welcome to home page</h1>
+        <button>Join Meeting</button>
+        <input type="text" placeholder="Enter Meeting ID" />
+        </>
+    )
+}
+
+export default Home;
